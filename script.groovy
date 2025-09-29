21c4c41b824bcc21711b43c65056e31e9b3b7833
Martins Love <martins_love18@yahoo.com>
2025-09-29 16:51:10 +0000

    Edit script.groovy

--- script.groovy
+++ script.groovy
@@ -1,13 +1,19 @@
-def buildAPP() {
+def buildJar() {
     echo 'building the application...'
+    sh 'mvn package'
 }
 
-def testAPP() {
-    echo 'testing the application...'
+def buildImage() {
+    echo 'building the application...'
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t snrmartins/java-maven-app:20-alpine .'
+        sh 'echo $PASS | docker login -u $USER --password-stdin'
+        sh 'docker push snrmartins/java-maven-app:20-alpine'
+    }
 }
 
 def deployAPP() {
     echo 'deploying the application...'
-    echo "deploying version ${params.VERSION}"
 }
+
 return this
